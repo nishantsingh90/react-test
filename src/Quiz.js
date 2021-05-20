d367179a0c130b8d65b5a99e8f11bcd98314e115
nishantsingh90 <54987202+nishantsingh90@users.noreply.github.com>
2021-05-20 17:30:30 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -51,71 +51,7 @@ export default function Quiz() {
     return <Text></Text>   
 }
 
-  
-//   const questions=[
-//     {
-//         "questionId": 1,
-//         "formId": 1,
-//         "question": "When was your first ultrasound?",
-//         "typeId": 3,
-//         "type": "datepicker",
-//         "text": "2020-05-01",
-//         "options": [],
-//         "Name": "",
-//     },
-//     {
-//       "questionId": 2,
-//       "formId": 1,
-//       "question": "Did your health care provider give you an estimated date of delivery (EDD) based on ultrasound?",
-//       "typeId": 4,
-//       "type": "radio",
-//       "text": "",
-//       "Name": "",
-//       "options": [
-//           {
-//               "optionId": 1,
-//               "text": "Yes",
-//               "checked": false,
-//               "questions": []
-//           },
-//           {
-//               "optionId": 2,
-//               "text": "No",
-//               "checked":true,
-//               "questions": [
-//                   {
-//                       "questionId": 6,
-//                       "formId": 1,
-//                       "question": "Did you have an ultrasound during this pregnancy ?",
-//                       "typeId": 6,
-//                       "type": "dropdown",
-//                       "text": "",
-//                       "Name": "",
-//                       "options": [
-//                           {
-//                               "optionId": 1,
-//                               "text": "Yes",
-//                               "checked": true,
-//                           },
-//                           {
-//                               "optionId": 2,
-//                               "text": "No",
-//                               "checked": false,
-//                           },
-//                           {
-//                               "optionId": 3,
-//                               "text": "I dont know",
-//                               "checked": false,
-//                           }
-
-//                       ]
-//                   }
-//               ]
-//           }
-//       ]
-//   }
-   
-// ];
+ 
 
 
 
@@ -131,9 +67,6 @@ export default function Quiz() {
          
         questions.map(question => (
          <>
-         
-
-         {/* <Text key={question.questionId}>{question.questionId}. {question.question}</Text> */}
 
         <Text > {question.question}</Text>
 
